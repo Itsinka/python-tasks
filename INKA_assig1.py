d62fb3bbc6f645a3c646bdf988b8fe4fc5784713
Inkarim <114926067+Itsinka@users.noreply.github.com>
2023-10-23 14:26:03 +0600

    Add files via upload

---
+++ INKA_assig1.py
@@ -0,0 +1,9 @@
+name = input("Please enter your name: ")
+surname = input("Please enter your surname: ")
+hobby = input("Please enter your hobby: ")
+
+print("Surname = {} Name = {} Hobby = {}".format(surname.capitalize(), name.capitalize(), hobby))
+
+# Метод .format() в Python используется для форматирования строк.
+# Он позволяет вставлять значения переменных внутрь строки, заменяя специальные метки-заполнители {}
+
